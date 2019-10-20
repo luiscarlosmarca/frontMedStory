4d72cf8686ef0cbbefc72aae6545971aeaed5bff
luiscarlosmarca <luiscarlosmarca@gmail.com>
2019-10-19 22:30:45 -0500

    inicio de formulario de creaion de pacientes

--- medstory/src/App.vue
+++ medstory/src/App.vue
@@ -1,13 +1,34 @@
 <template>
-  <div id="app">
-    
+  <div class="container-fluid" id="app">
+    <form @Submit.prevent="hi">
+      <h1 align="center">Creacion de pacientes</h1>
+      <h2 align="center">MedStory</h2>
+       <div class="form-group">
+        <label> Nombre del paciente</label>
+        <input class="form-control" v-model="nombre">
+      </div>
+      <div class="form-group">
+        <button class="btn btn-dark" type="submit">Guardar Paciente</button>
+      </div>
+       </div>
+
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'app'
 }
 </script>
 
-
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- medstory/src/main.js
+++ medstory/src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.config.productionTip = false
 
